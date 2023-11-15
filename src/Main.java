0c01acba0929ee2b744164a5fd390347b3f0e6e2
LisaFabro <fabro.lisa@gmail.com>
2023-11-15 16:26:14 +0100

    add es 1 corretto

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,21 @@ public class Main {
     //Scrivere una funzione che controlli se un numero int è in un determinato range
     //Se lo è ritorna true se non lo è lancia un'eccezione
     public static void main(String[] args){
-        int numInput = 100;
-        System.out.println("Il numero inserito rientra nel range? " + control(numInput));
+        //try{
+            boolean isInRange = checkRange(3,2,4);
+            System.out.println("Il numero inserito rientra nel range? " + isInRange);
+        //}catch (Exception e){
+            System.out.println("Errore in checkRange");
+        //}
+
+        System.out.println("Fine codice");
     }
-    public static boolean control(int num){
-        boolean response= true;
-        try{
-            int test = num;
-            //range 0-99
-            if (num < 0 || num >= 100){
-                throw new ArithmeticException("false");
-            }
-        } catch (ArithmeticException e){
-            response= false;
-        }
-        return response;
+    //try-catch non serve nell'es, va usato sulla parte di codice su cui penso
+    //possa crearsi un errore ed evito che crashi runnandolo
+    public static boolean checkRange(int num, int lower, int upper) throws Exception {
+        if (num < lower || num >= upper){
+            throw new Exception("true");
+        }else {
+            return true;}
     }
 }
